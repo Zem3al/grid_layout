eebf7d8081d1fc7412179e06cb42c8e0619bf9dc
Zem3al <54201169+Zem3al@users.noreply.github.com>
2021-05-13 00:47:42 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <input v-model="col">
+    <button @click.prevent="add" > ADD</button>
+    <grid :grids="grids"></grid>
+  </div>
+</template>
+
+<script>
+import grid from './components/grid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    grid
+  },
+  data () {
+    return {
+      grids: [],
+      col: 0,
+    }
+  },
+  mounted() {
+    this.grids.push({col: 3 })
+  },
+  methods: {
+    add() {
+      this.grids.push({col: parseInt(this.col)})
+      console.log(this.grids)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/grid.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="grid-container" :style="style(grid.col)"  v-for="(grid,i) in grids"  :key="i" >
+      <div class="grid-item"   v-for="i in grid.col" :key="i">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "grid",
+  props: {
+    grids: {
+      type: Array,
+      required: true
+    },
+  },
+  data() {
+    return {
+     a : []
+    }
+  },
+  methods: {
+    style(col) {
+        // eslint-disable-next-line no-unused-vars
+       return { 'grid-template-columns' : Array.from({length: col},  (_,i) => (i = 'auto ')).toString().replaceAll(',','')}
+    }
+  },
+}
+</script>
+
+<style scoped>
+.grid-container {
+  display: grid;
+  background: whitesmoke;
+  padding: 10px;
+}
+
+.grid-item {
+  border: 1px dashed rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+}
+</style>
